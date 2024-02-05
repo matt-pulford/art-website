876ac3fcc93cb3c7eab6070daee34be574d1a50f
Matt Pulford <mattyp666@outlook.com>
2024-02-05 15:26:25 -0500

    Add .env File, Add Error Page

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import path from 'path';
-import ejs from 'ejs';
-import nodemailer from 'nodemailer';
-import cookieParser from 'cookie-parser';
-import fs from 'fs';
+const express = require('express');
+const { fileURLToPath } = require('url');
+const path = require('path');
+const ejs = require('ejs');
+const nodemailer = require('nodemailer');
+const cookieParser = require('cookie-parser');
+const fs = require('fs');
+const dotenv = require('dotenv');
+const helmet = require('helmet'); // Added Helmet
+
+dotenv.config();
 
 // Constants
 const app = express();
-const port = 3000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 // Middleware to parse urlencoded data
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet()); // Use Helmet
 
 // Create nodemailer transporter
 const transporter = nodemailer.createTransport({
-  host: "smtp-mail.outlook.com", // Outlook SMTP server hostname
+  host: process.env.EMAIL_HOST, // Outlook SMTP server hostname
   secureConnection: false, // Set to false because TLS requires this to be false
-  port: 587, // Port for secure SMTP
+  port: process.env.EMAIL_PORT, // Port for secure SMTP
   tls: {
       ciphers: 'SSLv3'
   },
   auth: {
-      user: '', // Your Outlook email address
-      pass: '' // Your Outlook email password or app-specific password
+      user: process.env.EMAIL_USER, // Your Outlook email address
+      pass: process.env.EMAIL_PASSWORD // Your Outlook email password or app-specific password
   }
 });
-
 
 
 // Define routes
@@ -84,16 +83,13 @@ app.get('/image/:imageURL', (req, res) => {
   res.render('image', { image: { url: imageURL } });
 });
 
-
-
-
 // Contact Post Route
 app.post('/contact', (req, res) => {
   const { name, email, subject, message } = req.body;
 
   const mailOptions = {
-      from: '"HKC Contact " <mattyp666@outlook.com>', // Use the Outlook email address as the sender
-      to: 'mattpsender@gmail.com', // Recipient's email address
+      from: process.env.MAIL_FROM, // Use the Outlook email address as the sender
+      to: process.env.MAIL_TO, // Recipient's email address
       subject: subject,
       text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
   };
@@ -101,17 +97,22 @@ app.post('/contact', (req, res) => {
   transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
           console.error(error);
-          res.render('error', { loggedIn: req.session.loggedIn, user: req.session.userEmail })
+          res.render('error')
       } else {
           console.log('Email sent: ' + info.response);
-          res.render('contact', { loggedIn: req.session.loggedIn, user: req.session.userEmail });
+          res.render('contact');
 
       }
   });
 });
 
+// Error
+app.get('/error', (req, res) => {
+  res.render('error');
+});
+
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
